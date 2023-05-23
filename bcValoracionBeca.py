724c8f62f6499acb1902e2da89ca69c8087fd7ff
Alfonso Cabezas Fernández <alfonso.acf66@gmail.com>
2023-05-23 12:43:38 +0200

    Refact Beca

--- bcValoracionBeca.py
+++ bcValoracionBeca.py
@@ -61,61 +61,69 @@ class Persona(Clase):
 
 class Criterio(Regla):
     """
-    Define el criterio que seguira el sistema para aceptar o denegar una beca
+    Define el criterio que seguirá el sistema para aceptar o denegar una beca
     @param: Creditos >= 80 +0.2
-    @param: Creditos >=65 and < 80 +0.2
-    @param: Nota>=6.5 +0.2
-    @param: Renta<=20000 +0.2
-    @param: Independencia == true +0.2
+    @param: Creditos >= 65 y < 80 +0.2
+    @param: Nota >= 6.5 +0.2
+    @param: Renta <= 20000 +0.2
+    @param: Independencia == True +0.2
     @return: valor total de la solicitud.
     """
-    def __init__(self, idRegla):
 
+    def __init__(self, idRegla):
         Regla.__init__(self, idRegla)
 
     def execute(self, persona, solicitud):
         valor = 0.0
         for at in persona.atributos:
             if at.nombre == '%Creditos' and at.valor >= 80:
-                solicitud.descripcion += 'El atributo' + at.nombre + ' ' + 'con valor ' + str(at.valor) + ' aporta 0.7 puntos de valoracion\n'
-                valor = valor + 0.7
+                descripcion = f"El atributo {at.nombre} con valor {at.valor} aporta 0.7 puntos de valoración\n"
+                solicitud.descripcion += descripcion
+                valor += 0.7
 
-            if at.nombre == '%Creditos' and at.valor >= 65 and at.valor < 80:
-                solicitud.descripcion += 'El atributo' + at.nombre + ' ' + 'con valor ' + str(at.valor) + ' aporta 0.3 puntos de valoracion\n'
-                valor = valor + 0.3
+            if at.nombre == '%Creditos' and 65 <= at.valor < 80:
+                descripcion = f"El atributo {at.nombre} con valor {at.valor} aporta 0.3 puntos de valoración\n"
+                solicitud.descripcion += descripcion
+                valor += 0.3
 
             if at.nombre == 'Nota' and at.valor >= 6.5:
-                solicitud.descripcion += 'El atributo' + at.nombre + ' ' + 'con valor ' + str(at.valor) + ' aporta 0.4 puntos de valoracion\n'
-                valor = valor + 0.4
+                descripcion = f"El atributo {at.nombre} con valor {at.valor} aporta 0.4 puntos de valoración\n"
+                solicitud.descripcion += descripcion
+                valor += 0.4
 
             if at.nombre == 'Renta' and at.valor <= 20000:
-                solicitud.descripcion += 'El atributo' + at.nombre + ' ' + 'con valor ' + str(at.valor) + ' aporta 0.7 puntos de valoracion\n'
-                valor = valor + 0.7
+                descripcion = f"El atributo {at.nombre} con valor {at.valor} aporta 0.7 puntos de valoración\n"
+                solicitud.descripcion += descripcion
+                valor += 0.7
 
             if at.nombre == 'Independencia' and at.valor == True:
-                solicitud.descripcion += 'El atributo' + at.nombre + ' ' + 'con valor ' + str(at.valor) + ' aporta 0.3 puntos de valoracion\n'
-                valor = valor + 0.3
+                descripcion = f"El atributo {at.nombre} con valor {at.valor} aporta 0.3 puntos de valoración\n"
+                solicitud.descripcion += descripcion
+                valor += 0.3
 
-        solicitud.descripcion += 'El valor total de la solicitud obtenido es ' + str(valor) + '\n'
+        solicitud.descripcion += f"El valor total de la solicitud obtenido es {valor}\n"
         return valor, solicitud
+
 
 
 class AbstraerPorcentaje(Regla):
     """
-    Regla basica para abstraer porcentaje de aprobados
+    Regla básica para abstraer porcentaje de aprobados
     """
+
     def __init__(self, idRegla):
         Regla.__init__(self, idRegla)
 
     def execute(self, persona, solicitud):
-
         porcentaje = float(persona.atAsignaturasAprobadas.valor) / float(persona.atAsignaturasCursadas.valor) * 100
         asignado = False
+
         for i in persona.atributos:
             if i.nombre == '%Creditos':
                 i.valor = porcentaje
                 asignado = True
-        if asignado == False:
+
+        if not asignado:
             persona.atCreditosAprobados.valor = porcentaje
             persona.atributos.append(persona.atCreditosAprobados)
 
@@ -124,47 +132,40 @@ class AbstraerPorcentaje(Regla):
 
 class AbstraerLimite(Regla):
     """
-    Regla basica para abstraer el limite minimo para la concesion de la beca
+    Regla básica para abstraer el límite mínimo para la concesión de la beca
     """
+
     def __init__(self, idRegla):
         Regla.__init__(self, idRegla)
 
     def execute(self, persona, solicitud):
-
         for i in solicitud.atributos:
             if i.nombre == 'Tipo':
-                if i.valor == 'General':
-                    asignado = False
-                    solicitud.descripcion += 'A la beca de tipo general requiere un minimo de 1.0 puntos de valoracion para ser concedida\n'
-                    for j in solicitud.atributos:
-                        if j.nombre == 'ValorLimite':
-                            j.valor = 0.7
-                            asignado = True
-                    if asignado == False:
-                        solicitud.valorLimite.valor = 1.0
-                        solicitud.atributos.append(solicitud.valorLimite)
-                if i.valor == 'Movilidad':
-                    asignado = False
-                    solicitud.descripcion += 'A la beca de tipo movilidad requiere un minimo de 0.5 puntos de valoracion para ser concedida\n'
-                    for j in solicitud.atributos:
-                        if j.nombre == 'ValorLimite':
-                            j.valor = 0.5
-                            asignado = True
-                    if asignado == False:
-                        solicitud.valorLimite.valor = 0.5
-                        solicitud.atributos.append(solicitud.valorLimite)
-                if i.valor == 'Especial':
-                    asignado = False
-                    solicitud.descripcion += 'A la beca de tipo especial requiere un minimo de 0.9 puntos de valoracion para ser concedida \n'
-                    for j in solicitud.atributos:
-                        if j.nombre == 'ValorLimite':
-                            j.valor = 0.9
-                            asignado = True
-                    if asignado == False:
-                        solicitud.valorLimite.valor = 0.9
-                        solicitud.atributos.append(solicitud.valorLimite)
+                tipo_beca = i.valor
+                asignado = False
+                if tipo_beca == 'General':
+                    descripcion = 'A la beca de tipo general requiere un mínimo de 1.0 puntos de valoración para ser concedida\n'
+                    valor_limite = 0.7
+                elif tipo_beca == 'Movilidad':
+                    descripcion = 'A la beca de tipo movilidad requiere un mínimo de 0.5 puntos de valoración para ser concedida\n'
+                    valor_limite = 0.5
+                elif tipo_beca == 'Especial':
+                    descripcion = 'A la beca de tipo especial requiere un mínimo de 0.9 puntos de valoración para ser concedida\n'
+                    valor_limite = 0.9
+
+                for j in solicitud.atributos:
+                    if j.nombre == 'ValorLimite':
+                        j.valor = valor_limite
+                        asignado = True
+
+                if not asignado:
+                    solicitud.valorLimite.valor = valor_limite
+                    solicitud.atributos.append(solicitud.valorLimite)
+
+                solicitud.descripcion += descripcion
 
         return solicitud
+
 
 
 if __name__ == "__main__":
